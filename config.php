e934880dda8ec59cf8e8199bf46b7390ee0dc315
Nick Barone <xnickbarone@gmail.com>
2013-08-06 23:01:16 -0400

    Create config.php
    Hidden config file

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+$config = array(
+  'api' => array( // grooveshark api - you need permission to get an api key
+		'key' => '',
+		'secret' => '',
+	),
+	'tinysong' => array(
+		'key' => '',
+	),
+	'grooveshark' => array(
+		'username' => '',
+		'password' => '',
+	),
+);
+?>
