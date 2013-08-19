7f6695f2a1e4e1f16130d7559f4f87991e11bcd1
nbarone <nick@nbar1.com>
2013-08-19 19:13:37 -0400

    fixed #1, fixed #2: nickname view fixed

--- f.php
+++ f.php
@@ -9,39 +9,45 @@ $gs->api_key = $config['tinysong']['key'];
 $gsapi = new gsAPI($config['api']['key'], $config['api']['secret']);
 gsAPI::$headers = array("X-Client-IP: " . $_SERVER['REMOTE_ADDR']);
 
-switch($_REQUEST['f'])
+if(!isset($_REQUEST['f']))
 {
-	case 'initialize':
-		echo $gs->initializeView();
-		break;
-	case 'queue':
-		echo $gs->displayQueueView();
-		break;
-
-	case 'search':
-		echo $gs->displaySearchView($gs->doSearch($_REQUEST['query']));
-		break;
-
-	case 'setname':
-		$gs->setNickname($_REQUEST['nickname']);
-		echo $gs->initializeView();
-		break;
-
-	case 'add':
-		echo $gs->addSongToQueue($_REQUEST['songID'], $_REQUEST['songPriority'], $_REQUEST['songArtist'], $_REQUEST['songTitle']);
-		break;
-
-	case 'markSong30Seconds':
-		echo $gsapi->markStreamKeyOver30Secs($_REQUEST['streamKey'], $_REQUEST['streamServerID']);
-		break;
-	case 'getSongInfo':
-		echo $gsapi->getSongInfo($_REQUEST['song']);
-		break;
-	case 'getNextSong':
-		echo $gs->getNextSong();
-		break;
-	default:
-		echo $gs->initializeView();
-		break;
+	echo $gs->initializeView();
+}
+else {
+	switch($_REQUEST['f'])
+	{
+		case 'initialize':
+			echo $gs->initializeView();
+			break;
+		case 'queue':
+			echo $gs->displayQueueView();
+			break;
+	
+		case 'search':
+			echo $gs->displaySearchView($gs->doSearch($_REQUEST['query']));
+			break;
+	
+		case 'setname':
+			$gs->setNickname($_REQUEST['nickname']);
+			echo $gs->initializeView();
+			break;
+	
+		case 'add':
+			echo $gs->addSongToQueue($_REQUEST['songID'], $_REQUEST['songPriority'], $_REQUEST['songArtist'], $_REQUEST['songTitle']);
+			break;
+	
+		case 'markSong30Seconds':
+			echo $gsapi->markStreamKeyOver30Secs($_REQUEST['streamKey'], $_REQUEST['streamServerID']);
+			break;
+		case 'getSongInfo':
+			echo $gsapi->getSongInfo($_REQUEST['song']);
+			break;
+		case 'getNextSong':
+			echo $gs->getNextSong();
+			break;
+		default:
+			echo $gs->initializeView();
+			break;
+	}
 }
 ?>
