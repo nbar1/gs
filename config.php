a06dbefbc5408e1c3c111e884ea726be524e0e73
nbarone <nick@nbar1.com>
2013-09-19 20:28:46 -0400

    Updated config file

--- config.php
+++ config.php
@@ -1,15 +1,24 @@
 <?php
+/**
+ * Config entries for various individual installation variables
+ */
 $config = array(
-	'api' => array( // grooveshark api - you need permission to get an api key
-		'key' => '',
-		'secret' => '',
+	'api' => array( // Obtain a GrooveShark API key by requesting access at http://developers.grooveshark.com
+		'key' => '', // GrooveShark API key
+		'secret' => '', // GrooveShark API secret
 	),
 	'tinysong' => array(
-		'key' => '',
+		'key' => '', // TinySong API key
 	),
 	'grooveshark' => array(
-		'username' => '',
-		'password' => '', // md5 of grooveshark password
+		'username' => '', // GrooveShark username
+		'password' => '', // plaintext GrooveShark password
+	),
+	'database' => array(
+		'host' => '',
+		'user' => '',
+		'password' => '',
+		'db_name' => '',
 	),
 );
 ?>
