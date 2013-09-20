1714427898c2b4043885efbb5826f7647c4ed60f
nbarone <nick@nbar1.com>
2013-09-19 23:33:50 -0400

    Updated to js object, modal messaging, and css3 animations

--- assets/javascripts/global.js
+++ assets/javascripts/global.js
@@ -1,102 +1,162 @@
-var view = 'queue';
+var gs = gs || {};
 
-function loadView(data, setView, scrollToTop) {
-	$('#content_loader').html(data);
-	if(!scrollToTop) {
-		$(window).scrollTop(0);
-	}
-	view = setView;
-	return true;
-}
+gs = {
+	/**
+	 * Set default view
+	 */
+	view: 'queue',
 
-function reloadQueue() {
-	if(view == 'queue') {
-		$.post('f.php?f=queue', function(data) {
-			loadView(data, 'queue', 1);
+	/**
+	 * Initiate selected song
+	 */
+	selectedSong: Array(),
+
+	/**
+	 * Initialize default view
+	 */
+	init: function() {
+		gs.bind();
+		$.post('f.php?f=initialize', function(data) {
+			gs.loadView(data, 'queue');
+			gs.hideLoader();
 		});
-	}
-}
+	},
 
-function showLoader() {
-	$("#loader").show();
-}
-function hideLoader() {
-	$("#loader").hide();
-}
+	/**
+	 * Set up bindings
+	 */
+	bind: function() {
+		$('#search_submit').live('click', function() { gs.searchSubmit(); });
+		$('#search_input').bind('keypress', function(e) { gs.searchSubmitViaKeypress(e); });
+		$('#button_queue').live('click', function() { gs.returnToQueue(); });
+		$('.item_song').live('click', function() { gs.selectSong($(this)); });
+		$('#addToQueue_add').live('click', function() { gs.addToQueue(gs.selectedSong, "low"); });
+		$('#addToQueue_promote').live('click', function() { gs.addToQueue(gs.selectedSong, "high"); });
+		$('#setUser_submit').live('click', function() { gs.setUsername(); });
+		$('#setUser_textbox').bind('keypress', function(e) { gs.setUsernameViaKeypress(e); });
+	},
 
-/*
- * Load queue initially
- */
-$.post('f.php?f=initialize', function(data) {
-	loadView(data, 'queue');
-	hideLoader();
-});
+	/**
+	 * Load given data into a given view
+	 */
+	loadView: function(data, setView, scrollToTop) {
 
-/*
- * Search view
- */
-$('#search_submit').live('click', function() {
-	$('#search_input').blur();
-	showLoader();
-	$.post('f.php?f=search', { query : $('#search_input').val() }, function(data) {
-		loadView(data, 'search');
-		hideLoader();
-	});
-});
-$('#search_input').bind('keypress', function(e) {
-	if(e.keyCode==13){
+		$('#content_loader').html(data);
+		if(!scrollToTop) {
+			$(window).scrollTop(0);
+		}
+		gs.view = setView;
+		return true;
+	},
+
+	/**
+	 * Reload the queue
+	 */
+	reloadQueue: function() {
+		if(gs.view == 'queue') {
+			$.post('f.php?f=queue', function(data) {
+				gs.loadView(data, 'queue', 1);
+			});
+		}
+	},
+
+	/**
+	 * Submit a new search
+	 */
+	searchSubmit: function() {
 		$('#search_input').blur();
-		$('#search_submit').click();
+		gs.showLoader();
+		$.post('f.php?f=search', { query : $('#search_input').val() }, function(data) {
+			gs.loadView(data, 'search');
+			gs.hideLoader();
+		});
+	},
+
+	/**
+	 * Submit search via enter key
+	 */
+	searchSubmitViaKeypress: function(e) {
+		if(e.keyCode==13){
+			$('#search_input').blur();
+			$('#search_submit').click();
+		}
+	},
+
+	/**
+	 * Return to queue view from search view
+	 */
+	returnToQueue: function() {
+		gs.showLoader();
+		$.post('f.php?f=queue', function(data) {
+			gs.loadView(data, 'queue');
+			$('#search_input').val("");
+			gs.hideLoader();
+		});
+	},
+
+	/**
+	 * Select a song when tapped
+	 */
+	selectSong: function(song) {
+		var moreopts = song.children('.moreoptions');
+		gs.selectedSong = new Array(song.attr('rel'), song.children('div').children('.song_name').html(), song.children('div').children('.song_artist').html());
+		$('.moreoptions').css('height', '0');
+		if(moreopts.height() > 1) {
+			moreopts.css('height', '0');
+		} else {
+			moreopts.css('height', '45px');
+		}
+	},
+
+	/**
+	 * Add selected song to queue
+	 */
+	addToQueue: function(song, priority) {
+		$.post('f.php?f=add', {songID:song[0], songTitle:song[1], songArtist:song[2], songPriority:priority}, function(data) {
+			$('.moreopts').slideUp();
+			gs.showModal(data, 2500);
+		});
+	},
+
+	/**
+	 * Set username
+	 */
+	setUsername: function() {
+		$.post('f.php?f=setname', {set:"true", nickname:$('#setUser_textbox').val() }, function(data) {
+			gs.loadView(data, 'queue');
+		});
+	},
+
+	/**
+	 * Set username via enter key
+	 */
+	setUsernameViaKeypress: function(e) {
+		if(e.keyCode==13) {
+			$('#setUser_submit').click();
+		}
+	},
+	/**
+	 * Show loading modal
+	 */
+	showLoader: function() {
+		$("#loader").show();
+	},
+
+	/**
+	 * Hide loading modal
+	 */
+	hideLoader: function() {
+		$("#loader").hide();
+	},
+	/**
+	 * Show information modal
+	 */
+	showModal: function(data, time) {
+		$('#modal').html(data).css('opacity', '0.6');
+		var hideModal = setTimeout(function() {
+			$('#modal').css('opacity', '0');
+		}, time);
 	}
-});
-$('#button_queue').live('click', function() {
-	showLoader();
-	$.post('f.php?f=queue', function(data) {
-		loadView(data, 'queue');
-		$('#search_input').val("");
-		hideLoader();
-	});
-});
-
-/*
- * Add to queue options
- */
-$('.item_song').live('click', function() {
-	var moreopts = $(this).children('.moreoptions');
-	selectedSong = new Array($(this).attr('rel'), $(this).children('div').children('.song_name').html(), $(this).children('div').children('.song_artist').html());
-	$('.moreoptions').slideUp();
-	if(moreopts.is(':visible') == false) {
-		moreopts.slideDown();
-	} else {
-		moreopts.slideUp();
-	}
-});
-
-$('#addToQueue_add').live('click', function() {
-	addToQueue(selectedSong, "low");
-});
-
-$('#addToQueue_promote').live('click', function() {
-	addToQueue(selectedSong, "high");
-});
-
-function addToQueue(song, priority) {
-	console.log(song);
-	$.post('f.php?f=add', {songID:song[0], songTitle:song[1], songArtist:song[2], songPriority:priority}, function(data) {
-		$('.moreopts').slideUp();
-	});
 }
 
-/*
- * Set nickname view
- */
-$('#setUser_submit').live('click', function() {
-	$.post('f.php?f=setname', {set:"true", nickname:$('#setUser_textbox').val() }, function(data) {
-		loadView(data, 'queue');
-	});
-});
-$('#setUser_textbox').bind('keypress', function(e) {
-	if(e.keyCode==13) {
-		$('#setUser_submit').click();
-	}
-});
+gs.init();
