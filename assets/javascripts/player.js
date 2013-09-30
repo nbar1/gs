b986ebab71e3b4ba18f55097c39498c895d0205c
nbarone <nick@nbar1.com>
2013-09-29 23:42:22 -0400

    Updated player.js

--- assets/javascripts/player.js
+++ assets/javascripts/player.js
@@ -1,77 +1,123 @@
-function playSong(songID) {
-	if(songID == "") {
-		var t = setTimeout("nextSong()", 30000);
-		$('#currentSong .songTitle').html("No Songs Queued");
-		$('#currentSong .songArtist').html("");
-	} else {
-		$.ajax({
-			url: "/gs/player/stream/"+songID,
-			type: "GET",
-			success: function(response) {
-				console.log(response);
-				var responseData = $.parseJSON(response);
-				window.player.playStreamKey(responseData.StreamKey, responseData.StreamServerHostname, responseData.StreamServerID);
-				var streamKey = responseData.StreamKey;
-				var streamServerID = responseData.StreamServerID;
-				var streamTime = responseData.uSecs / 1000 - 0; // - seconds of lag, 0 seems to work fine
-				getSongInfo(songID);
-				var t = setTimeout("markSong30Seconds('"+streamKey+"', "+streamServerID+")", 45000);
-				var t = setTimeout("nextSong()", streamTime);
-			},
-			error: function() {
-				console.log("no songs to play, waiting 30 seconds");
-				var t = setTimeout("nextSong()", 30000);
-				$('#currentSong .songTitle').html("No Songs Queued");
-				$('#currentSong .songArtist').html("");
+var player = player || {};
+
+player = {
+	/**
+	 * Initialize
+	 */
+	init: function() {
+		player.bind();
+		player.setupPlayer();
+		var t = setTimeout("player.getNextSong()", 2000);
+	},
+
+	/**
+	 * Set up bindings
+	 */
+	bind: function() {
+		// nothing here until player controls
+	},
+
+	/**
+	 * Loads the flash component for the stream
+	 */
+	setupPlayer: function() {
+		swfobject.embedSWF("http://grooveshark.com/APIPlayer.swf", "player", "300", "300", "9.0.0", "", {}, {allowScriptAccess: "always"}, {id:"groovesharkPlayer", name:"groovesharkPlayer"}, function(e) {
+			var element = e.ref;
+			if (element)
+			{
+				setTimeout(function() {
+					window.gsplayer = element;
+					window.gsplayer.setVolume(99);
+				}, 1500);
 			}
 		});
+	},
+
+	/**
+	 * Gets the info for the given song and updated UI
+	 */
+	getSongInfo: function(token) {
+		$.ajax({
+			url: "/gs/song/"+token,
+			type: "GET",
+			success: function(response){
+				var songInfo = $.parseJSON(response);
+				$('#currentSong .songTitle').html(songInfo.title);
+				$('#currentSong .songArtist').html(songInfo.artist);
+			}
+		});
+	},
+
+	/**
+	 * Get token of next song
+	 */
+	getNextSong: function() {
+		$.ajax({
+			url: "/gs/player/next",
+			type: "GET",
+			success: function(response){
+				player.playSong(response);
+			}
+		});
+	},
+
+	/**
+	 * Get token of next song
+	 */
+	getNextSong: function() {
+		$.ajax({
+			url: "/gs/player/next",
+			type: "GET",
+			success: function(response){
+				player.playSong(response);
+			}
+		});
+	},
+
+	/**
+	 * Mark song at 30 seconds
+	 */
+	markSong30Seconds: function(streamKey, streamServerID) {
+		$.ajax({
+			url: "/gs/player/stream/validate",
+			type: "POST",
+			data: {
+				streamKey: streamKey,
+				streamServerID: streamServerID
+			}
+		});
+	},
+
+	/**
+	 * Play a given song
+	 */
+	playSong: function(token) {
+		if(token == "") {
+			var t = setTimeout("player.getNextSong()", 30000);
+			$('#currentSong .songTitle').html("No Songs Queued");
+			$('#currentSong .songArtist').html("");
+		} else {
+			$.ajax({
+				url: "/gs/player/stream/"+token,
+				type: "GET",
+				success: function(response) {
+					var responseData = $.parseJSON(response);
+					window.gsplayer.playStreamKey(responseData.StreamKey, responseData.StreamServerHostname, responseData.StreamServerID);
+					var streamKey = responseData.StreamKey;
+					var streamServerID = responseData.StreamServerID;
+					var streamTime = responseData.uSecs / 1000 - 0; // - seconds of lag, 0 seems to work fine
+					player.getSongInfo(token);
+					var t = setTimeout("player.markSong30Seconds('"+streamKey+"', "+streamServerID+")", 45000);
+					var t = setTimeout("player.getNextSong()", streamTime);
+				},
+				error: function() {
+					var t = setTimeout("player.getNextSong()", 30000);
+					$('#currentSong .songTitle').html("No Songs Queued");
+					$('#currentSong .songArtist').html("");
+				}
+			});
+		}
 	}
 }
 
-function nextSong() {
-	$.ajax({
-		url: "/gs/player/next",
-		type: "GET",
-		success: function(response){
-			console.log("nextsong: "+response);
-			playSong(response);
-		}
-	});
-}
-
-function getSongInfo(songID) {
-	$.ajax({
-		url: "/gs/song/"+songID,
-		type: "GET",
-		success: function(response){
-			console.log(response);
-			var songInfo = $.parseJSON(response);
-			$('#currentSong .songTitle').html(songInfo.title);
-			$('#currentSong .songArtist').html(songInfo.artist);
-		}
-	});
-}
-
-function markSong30Seconds(streamKey, streamServerID) {
-	$.ajax({
-		url: "/gs/player/stream/validate",
-		type: "POST",
-		data: {
-			streamKey: streamKey,
-			streamServerID: streamServerID
-		}
-	});
-}
-
-swfobject.embedSWF("http://grooveshark.com/APIPlayer.swf", "player", "300", "300", "9.0.0", "", {}, {allowScriptAccess: "always"}, {id:"groovesharkPlayer", name:"groovesharkPlayer"}, function(e) {
-	var element = e.ref;
-	if (element)
-	{
-		setTimeout(function() {
-			window.player = element;
-			window.player.setVolume(99);
-		}, 1500);
-	}
-});
-
-var t = setTimeout("nextSong()", 2000);
+player.init();
