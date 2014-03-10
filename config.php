ddab8fcc862fb9823f393861578e51736c731cc6
Nick Barone <nick@nbar1.com>
2014-03-10 19:22:26 -0400

    Quick fix

---
+++ config.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Config entries for various individual installation variables
+ */
+$config = array(
+	'autoplay' => true,
+	'api' => array( // Obtain a GrooveShark API key by requesting access at http://developers.grooveshark.com
+		'key' => '', // GrooveShark API key
+		'secret' => '', // GrooveShark API secret
+	),
+	'tinysong' => array(
+		'key' => '', // TinySong API key
+	),
+	'grooveshark' => array(
+		'username' => '', // GrooveShark username
+		'password' => '', // plaintext GrooveShark password
+	),
+	'database' => array(
+		'host' => '',
+		'user' => '',
+		'password' => '',
+		'db_name' => '',
+	),
+?>
