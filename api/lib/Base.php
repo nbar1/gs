5b1f7f8552d92a5532eb8bfdedf67aaf050a5f66
Nick Barone <xnickbarone@gmail.com>
2014-04-09 23:25:51 -0400

    Updated base class with new config pointers

--- api/lib/Base.php
+++ api/lib/Base.php
@@ -7,11 +7,6 @@
 class Base
 {
 	/**
-	 * Config
-	 */
-	public $config;
-
-	/**
 	 * Data access object
 	 */
 	private $dao;
@@ -47,40 +42,6 @@ class Base
 	public $autoplay;
 
 	/**
-	 * Template engine
-	 */
-	public $templateEngine;
-
-	/**
-	 * Constructor
-	 *
-	 * Initiate the database object
-	 */
-	public function __construct()
-	{
-		$this->config =  array(
-			'autoplay' => true,
-			'api' => array( // Obtain a GrooveShark API key by requesting access at http://developers.grooveshark.com
-				'key' => 'nbar1', // GrooveShark API key
-				'secret' => '1f64634987618265edb26fe236c00011', // GrooveShark API secret
-			),
-			'tinysong' => array(
-				'key' => 'a2e5ffd9cc99a2bee6207e4921def6a7', // TinySong API key
-			),
-			'grooveshark' => array(
-				'username' => 'nbarone', // GrooveShark username
-				'password' => '17543366', // plaintext GrooveShark password
-			),
-			'database' => array(
-				'host' => 'localhost',
-				'user' => 'root',
-				'password' => 'nick',
-				'db_name' => 'nbar1_gs',
-			),
-		);
-	}
-
-	/**
 	 * Get data access object
 	 *
 	 * @return Dao Data access object
@@ -103,7 +64,7 @@ class Base
 	{
 		if (!isset($this->gsapi))
 		{
-			$this->gsapi = new gsAPI($this->config['api']['key'], $this->config['api']['secret']);
+			$this->gsapi = new gsAPI(GSAPI_KEY, GSAPI_SECRET);
 		}
 		return $this->gsapi;
 	}
@@ -200,7 +161,7 @@ class Base
 		else {
 			$_SESSION['gsCountry'] = $this->getGsAPI()->getCountry();
 		}
-		$this->getGsAPI()->authenticate($this->config['grooveshark']['username'], $this->config['grooveshark']['password']);
+		$this->getGsAPI()->authenticate(GROOVESHARK_USERNAME, GROOVESHARK_PASSWORD);
 	}
 }
 ?>
